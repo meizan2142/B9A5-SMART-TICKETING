27e801b8173df10d0c3eedd78b1031dd7fa95d46
Your Name01~git <saifsahai2104@gmail.com>
2024-02-20 00:50:51 +0600

    scroll behaviour added successfully on Buy Tickets button

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("hey, Bro. How are you?");
+let seatsLeft = document.getElementById('seat-left')
+console.log(seatsLeft.innerText);
+let seatsCount = document.getElementById('seats-count')
+console.log(seatsCount.innerText);
+document.getElementById('apply-btn').addEventListener('click', function (e) {
+    console.log("hey bro, what are doing?");
+})
+document.getElementById('next-btn').addEventListener('click', function (e) {
+    console.log("hey bro, what are doing?");
+})
