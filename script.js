159395be8f800a3289493a9043e8b52f3f985c0b
Your Name01~git <saifsahai2104@gmail.com>
2024-02-20 21:22:10 +0600

    Design completed

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
-let seatsLeft = document.getElementById('seat-left')
-console.log(seatsLeft.innerText);
-let seatsCount = document.getElementById('seats-count')
-console.log(seatsCount.innerText);
-document.getElementById('apply-btn').addEventListener('click', function (e) {
-    console.log("hey bro, what are doing?");
-})
-document.getElementById('next-btn').addEventListener('click', function (e) {
-    console.log("hey bro, what are doing?");
-})
+// let seatsLeft = document.getElementById('seat-left')
+// console.log(seatsLeft.innerText);
+// let seatsCount = document.getElementById('seats-count')
+// console.log(seatsCount.innerText);
+// document.getElementById('apply-btn').addEventListener('click', function (e) {
+//     console.log("hey bro, what are doing?");
+// })
+// document.getElementById('next-btn').addEventListener('click', function (e) {
+//     console.log("hey bro, what are doing?");
+// })
+
+// const seatList = document.getElementsByClassName('seat-list').addEventListener("click", function () {
+//     const seatName = document.get
+// });
+
